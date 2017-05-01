19132241bcf63131c97d0a748bd407e6471b0a6f
Glover, Robert <robert.glover@kronos.com>
2017-04-30 22:18:25 -0700

    Initial Spring Rest Quick Poll implementation

---
+++ quickpoll/src/main/java/com/apress/repository/PollRepository.java
@@ -0,0 +1,8 @@
+package com.apress.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.apress.domain.Poll;
+
+public interface PollRepository extends CrudRepository<Poll, Long> {
+
+}
