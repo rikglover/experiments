e8f2b40fbd4043f91ee6c5e3bb4db15c37527430
Glover, Robert <robert.glover@kronos.com>
2017-05-05 12:53:47 -0700

    Adding swagger ui

---
+++ quickpoll/src/main/java/com/apress/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.apress;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.models.dto.builder.ApiInfoBuilder;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.inject.Inject;
+
+@Configuration
+@EnableSwagger
+public class SwaggerConfig {
+
+	@Inject
+	private SpringSwaggerConfig springSwaggerConfig;
+
+	@Bean
+	public SwaggerSpringMvcPlugin configureSwagger() {
+
+		SwaggerSpringMvcPlugin swaggerSpringMvcPlugin = new SwaggerSpringMvcPlugin(this.springSwaggerConfig);
+
+		ApiInfo apiInfo = new ApiInfoBuilder()
+				.title("QuickPoll REST API")
+				.description("QuickPoll Api for creating and managing polls")
+				.termsOfServiceUrl("http://example.com/terms-of-service")
+				.contact("info@example.com")
+				.license("MIT License")
+				.licenseUrl("http://opensource.org/licenses/MIT")
+				.build();
+
+		swaggerSpringMvcPlugin
+				.apiInfo(apiInfo)
+				.apiVersion("1.0")
+				.includePatterns("/polls/*.*", "/votes/*.*", "/computeresult/*.*");
+
+		return swaggerSpringMvcPlugin;
+	}
+}
