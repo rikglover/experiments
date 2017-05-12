2cd9b49a74da356cc9804763460019e9c3a65658
Glover, Robert <robert.glover@kronos.com>
2017-05-11 22:22:16 -0700

    Updating per chapter 8 (basic auth)

---
+++ quickpoll/src/main/java/com/apress/security/QuickPollUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.apress.security;
+
+import com.apress.domain.User;
+import com.apress.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class QuickPollUserDetailsService implements UserDetailsService {
+
+	@Inject
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = userRepository.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException(String.format("User with the username %s doesn't exist", username));
+		}
+
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		if (user.isAdmin()) {
+			authorities = AuthorityUtils.createAuthorityList("ROLE_ADMIN");
+		}
+		// Create a UserDetails object from the data
+		UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+
+		return userDetails;
+	}
+}
---
+++ quickpoll/src/main/java/com/apress/v3/controller/VoteController.java
@@ -0,0 +1,50 @@
+package com.apress.v3.controller;
+
+import com.apress.domain.Vote;
+import com.apress.repository.VoteRepository;
+import com.wordnik.swagger.annotations.Api;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.inject.Inject;
+import java.net.URI;
+
+@RestController("voteControllerV3")
+@RequestMapping("/v3/")
+@Api(value = "votes", description = "Votes API")
+public class VoteController {
+
+	@Inject
+	private VoteRepository voteRepository;
+
+	@RequestMapping(value = "/polls/{pollId}/votes", method = RequestMethod.POST)
+	public ResponseEntity<?> createVote(@PathVariable Long pollId, @RequestBody Vote vote) {
+
+		vote = voteRepository.save(vote);
+
+		// Set the headers for the newly created resource
+		HttpHeaders responseHeaders = new HttpHeaders();
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(vote.getId())
+				.toUri();
+
+		responseHeaders.setLocation(uri);
+
+		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/polls/{pollId}/votes", method = RequestMethod.GET)
+	public Iterable<Vote> getAllVotes(@PathVariable Long pollId) {
+
+		return voteRepository.findByPoll(pollId);
+	}
+}
