9661ec1aa94327959551119d6c90755383218298
rikglover <rikglover@gmail.com>
2017-05-17 22:51:06 -0700

    Chapter 9, client code completed

--- quickpoll/src/main/java/com/apress/client/QuickPollClient.java
+++ quickpoll/src/main/java/com/apress/client/QuickPollClient.java
@@ -3,37 +3,70 @@ package com.apress.client;
 import com.apress.domain.Option;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import com.apress.domain.Poll;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 
 public class QuickPollClient {
 
     private static final String QUICK_POLL_URI_V1 = "http://localhost:8080/v1/polls";
+    private static final String QUICK_POLL_URI_V2 = "http://localhost:8080/v2/polls";
+    private static final String QUICK_POLL_URI_V3 = "http://localhost:8080/v3/polls";
+    private static final String QUICK_POLL_URI_V3_OAUTH = "http://localhost:8080/oauth2/v3/polls";
 
     private RestTemplate restTemplate = new RestTemplate();
+
+
+    private OAuth2RestTemplate restTemplate() {
+
+        List<String> scopes = Arrays.asList("read", "write");
+
+        ResourceOwnerPasswordResourceDetails resourceDetails = new ResourceOwnerPasswordResourceDetails();
+
+        resourceDetails.setGrantType("password");
+        resourceDetails.setAccessTokenUri("http://localhost:8080/oauth/token");
+        resourceDetails.setClientId("quickpolliOSClient");
+        resourceDetails.setClientSecret("top_secret");
+        resourceDetails.setScope(scopes);
+        resourceDetails.setUsername("mickey");
+        resourceDetails.setPassword("cheese");
+
+        return new OAuth2RestTemplate(resourceDetails);
+    }
 
     public Poll getPollById(Long pollId) {
 
         return restTemplate.getForObject(QUICK_POLL_URI_V1 + "/{pollId}", Poll.class, pollId);
     }
 
+    public Poll getPollByIdWithOauth(Long pollId) {
+
+        OAuth2RestTemplate restTemplate = restTemplate();
+
+        return restTemplate.getForObject(QUICK_POLL_URI_V3_OAUTH + "/{pollId}", Poll.class, pollId);
+    }
+
     public List<Poll> getAllPolls() {
 
-        ParameterizedTypeReference<List<Poll>> responseType = new ParameterizedTypeReference<List<Poll>>() { };
+        ParameterizedTypeReference<List<Poll>> responseType = new ParameterizedTypeReference<List<Poll>>() {
+        };
 
         ResponseEntity<List<Poll>> responseEntity = restTemplate.exchange(QUICK_POLL_URI_V1, HttpMethod.GET, null, responseType);
 
@@ -61,7 +94,35 @@ public class QuickPollClient {
 
     public void deletePoll(Long pollId) {
 
-        restTemplate.delete(QUICK_POLL_URI_V1 + "/{pollId}",  pollId);
+        restTemplate.delete(QUICK_POLL_URI_V1 + "/{pollId}", pollId);
+    }
+
+    public void deletePollV3(Long pollId) {
+
+        HttpHeaders authenticationHeaders = getAuthenticationHeader("admin", "admin");
+
+        restTemplate.exchange(QUICK_POLL_URI_V3 + "/{pollId}", HttpMethod.DELETE, new HttpEntity<Void>(authenticationHeaders), Void.class, pollId);
+    }
+
+    public PageWrapper<Poll> getAllPolls(int page, int size) {
+
+        ParameterizedTypeReference<PageWrapper<Poll>> responseType = new ParameterizedTypeReference<PageWrapper<Poll>>() {
+        };
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(QUICK_POLL_URI_V2).queryParam("page", page).queryParam("size", size);
+
+        ResponseEntity<PageWrapper<Poll>> responseEntity = restTemplate.exchange(builder.build().toUri(), HttpMethod.GET, null, responseType);
+
+        return responseEntity.getBody();
+    }
+
+    private HttpHeaders getAuthenticationHeader(String username, String password) {
+
+        String credentials = username + ":" + password;
+        byte[] base64CredentialData = Base64.getEncoder().encode(credentials.getBytes());
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Basic " + new String(base64CredentialData));
+        return headers;
     }
 
     public static void main(String[] args) {
@@ -85,10 +146,19 @@ public class QuickPollClient {
 
         client.updatePoll(newPoll2);
 
-        Poll newPoll3 = client.getPollById(newPoll2.getId());
+        Poll newPoll3 = client.getPollByIdWithOauth(newPoll2.getId());
 
         System.out.println("new new new poll: " + newPoll3);
 
-        client.deletePoll(newPoll3.getId());
+        client.deletePollV3(newPoll3.getId());
+
+        PageWrapper<Poll> pollWrapper1 = client.getAllPolls(0, 4);
+
+        pollWrapper1.getContent().forEach(System.out::println);
+
+        PageWrapper<Poll> pollWrapper2 = client.getAllPolls(1, 4);
+
+        pollWrapper2.getContent().forEach(System.out::println);
+
     }
 }
